1d511697a9e271fdb92584d9e8b1d354cb520d11
BIGBen <bvef@hotmail.com>
2015-04-10 11:46:56 +0200

    Update and rename LM35DZ/LM35DZ.cpp to LM35/LM35.cpp

--- LM35/LM35.cpp
+++ LM35/LM35.cpp
@@ -1,31 +1,28 @@
 /*
-  lm35DZ.cpp - Library for LM35DZ sensor.
+  lm35.cpp - Library for LM35 sensor.
   Created by Benoit Didry, January 31, 2015.
   Released into the public domain.
-
-  20150203 : remove getTemperature method and add getKelvin, getCelcius, getFahrenheit
 */
-
 #include "Arduino.h"
-#include "LM35DZ.h"
+#include "LM35.h"
 
-LM35DZ::LM35DZ(int pin) {
+LM35::LM35(int pin) {
   _pin = pin;
 }
 
-float LM35DZ::getKelvin() {
+float LM35::getKelvin() {
   analogRead(_pin);
   delay(10);
   return analogRead(_pin) * 500.0 / 1023.0 + 273.15; 
 }
 
-float LM35DZ::getCelcius() {
+float LM35::getCelcius() {
   analogRead(_pin);
   delay(10);
   return analogRead(_pin) * 500.0 / 1023.0;
 }
 
-float LM35DZ::getFahrenheit() {
+float LM35::getFahrenheit() {
   analogRead(_pin);
   delay(10);
   return analogRead(_pin) * 300.0 / 341.0 + 32; 
